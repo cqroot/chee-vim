f4e735c8191b705478788b0560c8c14112b59e81
cqroot <cqroot@outlook.com>
2022-11-05 22:10:07 +0800

    feat(outline): disable ESC from closing the outline window

--- lua/plugins/symbols-outline.lua
+++ lua/plugins/symbols-outline.lua
@@ -1,7 +1,11 @@
 local M = {}
 
 function M.load()
-	require("symbols-outline").setup()
+	require("symbols-outline").setup({
+		keymaps = { -- These keymaps can be a string or a table for multiple keys
+			close = { "q" },
+		},
+	})
 
 	vim.cmd("autocmd BufEnter * if (winnr(\"$\") == 1 && &filetype == 'Outline') | q | endif")
 end
