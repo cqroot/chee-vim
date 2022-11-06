2c81f4891c4fac5b7dafd765df6762f2187baf6b
cqroot <cqroot@outlook.com>
2022-11-06 21:17:03 +0800

    feat: support fcitx input method automatic switching

--- lua/cheevim.lua
+++ lua/cheevim.lua
@@ -3,6 +3,9 @@ local M = {}
 function M.init()
 	require("core.options").load()
 	require("core.mappings").load()
+	if require("core.utils").get("editor_im_switch") == "fcitx" then
+		require("core.im_fcitx").load()
+	end
 	require("plugins").load()
 	require("plugins.cmp").load()
 end
--- lua/core/default_options.lua
+++ lua/core/default_options.lua
@@ -4,6 +4,9 @@ return {
 	editor_relativenumber = false,
 	editor_cursorcolumn = false,
 
+	-- Options: [ "", "fcitx" ]
+	editor_im_switch = "",
+
 	format_on_save = false,
 	git_current_line_blame = false,
 	markdown_preview_browser = "surf",
