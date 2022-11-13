61a5fd55c0fcd12ebd91835883bb8e750927d5a3
cqroot <cqroot@outlook.com>
2022-11-14 05:47:40 +0800

    feat(null-ls): add formatter for cmake

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -3,7 +3,7 @@ local M = {}
 function M.load()
 	local null_ls = require("null-ls")
 	local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-	vim.lsp.buf.format({ timeout_ms = 2000 })
+	vim.lsp.buf.format({ timeout_ms = 5000 })
 
 	null_ls.setup({
 		on_attach = function(client, bufnr)
@@ -20,6 +20,7 @@ function M.load()
 		end,
 		sources = {
 			null_ls.builtins.formatting.clang_format,
+			null_ls.builtins.formatting.cmake_format,
 			null_ls.builtins.formatting.prettier,
 			null_ls.builtins.formatting.shfmt,
 			null_ls.builtins.formatting.stylua,
