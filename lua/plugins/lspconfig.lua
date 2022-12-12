3122c543418ed14339f7cc5186fab7816465bbe7
cqroot <cqroot@outlook.com>
2022-12-12 18:45:14 +0800

    feat: add support for bashls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -4,6 +4,10 @@ function M.load()
 	-- Set up lspconfig.
 	local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+	require("lspconfig").bashls.setup({
+		capabilities = capabilities,
+	})
+
 	require("lspconfig").clangd.setup({
 		capabilities = capabilities,
 	})
