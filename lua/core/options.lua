b9a4342c7fbcf954f5b499c3bf97130d0b796e3e
cqroot <cqroot@outlook.com>
2023-01-28 18:13:19 +0800

    feat: remove utils library

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,35 +1,47 @@
 local M = {}
 
+local function get_option(option)
+    local ok, _ = pcall(require, "custom_options")
+    if ok then
+        local value = require("custom_options")[option]
+        if value ~= nil then
+            return value
+        end
+    end
+
+    return require("default_options")[option]
+end
+
 function M.editor_LineNumber()
-    return require("core.utils").get("editor_LineNumber")
+    return get_option("editor_LineNumber")
 end
 
 function M.editor_CursorLine()
-    return require("core.utils").get("editor_CursorLine")
+    return get_option("editor_CursorLine")
 end
 
 function M.editor_CursorColumn()
-    return require("core.utils").get("editor_CursorColumn")
+    return get_option("editor_CursorColumn")
 end
 
 function M.editor_FormatOnSave()
-    return require("core.utils").get("editor_FormatOnSave")
+    return get_option("editor_FormatOnSave")
 end
 
 function M.editor_ImSwitch()
-    return require("core.utils").get("editor_ImSwitch")
+    return get_option("editor_ImSwitch")
 end
 
 function M.workbench_ColorScheme()
-    return require("core.utils").get("workbench_ColorScheme")
+    return get_option("workbench_ColorScheme")
 end
 
 function M.git_CurrentLineBlame()
-    return require("core.utils").get("git_CurrentLineBlame")
+    return get_option("git_CurrentLineBlame")
 end
 
 function M.markdown_PreviewBrowser()
-    return require("core.utils").get("markdown_PreviewBrowser")
+    return get_option("markdown_PreviewBrowser")
 end
 
 return M
--- scripts/generate.sh
+++ scripts/generate.sh
@@ -6,8 +6,20 @@ ROOT_PATH=$(dirname "$SCRIPT_PATH")
 main() {
 	local default_options_lua="$ROOT_PATH/lua/default_options.lua"
 
-    echo 'local M = {}'
-    echo
+    echo 'local M = {}
+
+local function get_option(option)
+    local ok, _ = pcall(require, "custom_options")
+    if ok then
+        local value = require("custom_options")[option]
+        if value ~= nil then
+            return value
+        end
+    end
+
+    return require("default_options")[option]
+end
+'
 
 	while read -r line; do
 		if [[ $line == M.* ]]; then
@@ -15,7 +27,7 @@ main() {
             option=$(echo "$line" | sed -n 's/^M\.\(.*\)\(=.*\)$/\1/p' | xargs)
 
             echo "function M.$option()"
-            echo "    return require(\"core.utils\").get(\"$option\")"
+            echo "    return get_option(\"$option\")"
             echo "end"
             echo
 		fi
