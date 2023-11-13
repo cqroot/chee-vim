90f9fbe8ef94c81fb00c82207d92b4dd581146ce
Keith Chu <cqroot@outlook.com>
2023-11-14 01:30:48 +0800

    feat: use neoformat instead of null-ls

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -130,17 +130,6 @@ function M.load()
                 require("plugins.mason-lspconfig").load()
             end,
         },
-        {
-            "jay-babu/mason-null-ls.nvim",
-            event = { "BufReadPre", "BufNewFile" },
-            dependencies = {
-                "williamboman/mason.nvim",
-                "jose-elias-alvarez/null-ls.nvim",
-            },
-            config = function()
-                require("plugins.mason-null-ls").load()
-            end,
-        },
 
         -- ************************************************************************
         -- *  Completion                                                          *
