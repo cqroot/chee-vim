50ac33ce5b775839dc4b897c89454e9997738600
Keith Chu <cqroot@outlook.com>
2024-09-12 02:01:58 +0800

    feat(plugins): add todo-comments

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,6 +50,15 @@ local plugins = {
 
     require("plugins.conform"),
     require("plugins.comment"),
+    {
+        "folke/todo-comments.nvim",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+    },
 
     -- ************************************************************************
     -- *  Git                                                                 *
